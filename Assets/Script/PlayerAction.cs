6cfadf31b1d173417c5e0895d7fcab969764227b
이원찬 <105588857+Leewonchan14@users.noreply.github.com>
2023-02-18 18:18:57 +0900

    애니메이션 생성 및 컨트롤러 추가

--- Assets/Script/PlayerAction.cs
+++ Assets/Script/PlayerAction.cs
@@ -5,12 +5,19 @@ using UnityEngine.InputSystem;
 
 public class PlayerAction : MonoBehaviour
 {
-    Vector2 inputVec;
+    private float h;
+    private float v;
+    public bool isHorizonMove;
     public float speed;
+    public Vector2 inputVec;
+
+    Vector2 curVec;
     Rigidbody2D rigid;
+    Animator anim;
     // Start is called before the first frame update
     private void Awake() {
         rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
     void Start()
     {
@@ -20,11 +27,33 @@ public class PlayerAction : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+        HorizonCheck();
     }
     void FixedUpdate() {
+        //십자이동 구현
+        inputVec = isHorizonMove?Vector2.right*h:Vector2.up*v;
+        if(curVec != inputVec){
+            AnimCheck();
+        }
+        curVec = inputVec;
         rigid.velocity = inputVec*speed;
     }
-    void OnMove(InputValue value){
-        inputVec = value.Get<Vector2>();
+    void HorizonCheck(){
+        bool hDown = Input.GetButtonDown("Horizontal");
+        bool vDown = Input.GetButtonDown("Vertical");
+        bool hUp = Input.GetButtonUp("Horizontal");
+        bool vUp = Input.GetButtonUp("Vertical");
+        //수직눌리거나 || 수평양방향이 눌리고있을때 한 방향이 떼질때 
+        if(vDown || (hUp&&h==0)) {isHorizonMove = false;}
+        else if(hDown || (vUp&&v==0)) {isHorizonMove = true;}
+    }
+    private void LateUpdate() {
+        
+    }
+    void AnimCheck(){
+        anim.SetInteger("HAxisRaw",(int)inputVec.x);
+        anim.SetInteger("VAxisRaw",(int)inputVec.y);
     }
 }
