477247d64ed9139682fe98cf3c60724a8f1cfb49
이원찬 <105588857+Leewonchan14@users.noreply.github.com>
2023-02-18 15:34:29 +0900

    타일맵 아틀라스를 생성, 가로,세로줄 생기던거 삭제

--- Assets/Script/PlayerAction.cs
+++ Assets/Script/PlayerAction.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class PlayerAction : MonoBehaviour
 {
+    public float h;
+    public float v;
+    public float speed;
+    Rigidbody2D rigid;
     // Start is called before the first frame update
+    private void Awake() {
+        rigid = GetComponent<Rigidbody2D>();
+    }
     void Start()
     {
         
@@ -13,6 +20,10 @@ public class PlayerAction : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+    }
+    void FixedUpdate() {
+        rigid.velocity = new Vector2(h,v)*speed;
     }
 }
