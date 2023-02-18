6f36d7700de5887b2c4cc189e9ae5c2df86f97ee
이원찬 <105588857+Leewonchan14@users.noreply.github.com>
2023-02-18 23:07:14 +0900

    npc,상자,데스크 추가 , 플레이어앞 조사 함수 구현

--- Assets/Script/PlayerAction.cs
+++ Assets/Script/PlayerAction.cs
@@ -1,28 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerAction : MonoBehaviour
 {
     private float h;
     private float v;
-    public bool isChange;
-    public bool isHorizonMove;
+    bool isHorizonMove;
+    [Header("#PlayerInfo")]
     public float speed;
-    public Vector2 inputVec;
+    [Header("#Player_Scanner")]
+    public RaycastHit2D front_Hit;
 
-    Vector2 curVec;
-    Rigidbody2D rigid;
-    Animator anim;
+    public Vec vec = new Vec();
+    public Scan scan = new Scan();
+    public Component compo = new Component();
+    
     // Start is called before the first frame update
     private void Awake() {
-        rigid = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
+        compo.rigid = GetComponent<Rigidbody2D>();
+        compo.anim = GetComponent<Animator>();
     }
     void Start()
     {
-        curVec = Vector2.zero;
     }
 
     // Update is called once per frame
@@ -30,28 +30,57 @@ public class PlayerAction : MonoBehaviour
     {
         h = Input.GetAxisRaw("Horizontal");
         v = Input.GetAxisRaw("Vertical");
-        HorizonCheck();
+        Debug.DrawRay(transform.position,vec.dir, Color.white, 0, true);
+        DirCheck();
         AnimCheck();
+        if(Input.GetButtonDown("Jump")){
+
+        }
+        FrontCheck();
     }
     void FixedUpdate() {
         //십자이동 구현
-        inputVec = isHorizonMove?Vector2.right*h:Vector2.up*v;
-        rigid.velocity = inputVec*speed;
+        vec.input = isHorizonMove?Vector2.right*h:Vector2.up*v;
+        compo.rigid.velocity = vec.input*speed;
     }
-    void HorizonCheck(){
+    void DirCheck(){
         bool hDown = Input.GetButtonDown("Horizontal");
         bool vDown = Input.GetButtonDown("Vertical");
         bool hUp = Input.GetButtonUp("Horizontal");
         bool vUp = Input.GetButtonUp("Vertical");
+
+
         //수직눌리거나 || 수평양방향이 눌리고있을때 한 방향이 떼질때 
         if(vDown || (hUp&&h==0)) {isHorizonMove = false;}
         else if(hDown || (vUp&&v==0)) {isHorizonMove = true;}
+
+        if(vec.input != Vector2.zero) vec.dir = vec.input;
     }
     private void LateUpdate() {
         
     }
     void AnimCheck(){
-        anim.SetInteger("HAxisRaw",(int)inputVec.x);
-        anim.SetInteger("VAxisRaw",(int)inputVec.y);
+        compo.anim.SetInteger("HAxisRaw",(int)vec.input.x);
+        compo.anim.SetInteger("VAxisRaw",(int)vec.input.y);
     }
+    void FrontCheck(){
+        front_Hit = Physics2D.Raycast(transform.position, vec.dir, 0.5f, scan.Layer);
+        scan.Object = front_Hit?front_Hit.collider.gameObject:null;
+    }
+}
+[System.Serializable]
+public class Vec{
+    public Vector2 input;
+    public Vector2 dir;
+}
+
+[System.Serializable]
+public class Scan{
+    public LayerMask Layer;
+    public GameObject Object;
+}
+[System.Serializable]
+public class Component{
+    public Rigidbody2D rigid;
+    public Animator anim;
 }
