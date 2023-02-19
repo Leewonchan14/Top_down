f43945157c0c6fb45d06ce6cb60bd10b37485bcc
이원찬 <105588857+Leewonchan14@users.noreply.github.com>
2023-02-19 17:16:55 +0900

    npc B 대화 설정

--- Assets/Script/TalkManager.cs
+++ Assets/Script/TalkManager.cs
@@ -11,6 +11,7 @@ public class TalkManager : MonoBehaviour
     }
     void GeneratorData(){
         talkData.Add(1,new string[] {"안녕!","하이루~","어서와~","무슨일이야?"});
+        talkData.Add(2,new string[] {"ㅋㅋㅋㅋ","헬로"});
         talkData.Add(3,new string[] {"평범한 나무상자이다."});
         talkData.Add(4,new string[] {"누군가 사용한 흔적이 있는 탁자이다."});
     }
