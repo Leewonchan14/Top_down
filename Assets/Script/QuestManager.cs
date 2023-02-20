c8d41ed2e8167363bce02dc6d13b973883238469
이원찬 <105588857+Leewonchan14@users.noreply.github.com>
2023-02-20 17:16:07 +0900

    퀘스트 진행에 따른 함수들 구현

--- Assets/Script/QuestManager.cs
+++ Assets/Script/QuestManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class QuestManager : MonoBehaviour
 {
     public int questId;
+    public int questActionIndex;
     Dictionary<int,QuestData> questList;
     // Start is called before the first frame update
     private void Awake() {
@@ -13,7 +14,7 @@ public class QuestManager : MonoBehaviour
     }
     void Start()
     {
-        
+        CheckQuest();
     }
 
     // Update is called once per frame
@@ -23,8 +24,26 @@ public class QuestManager : MonoBehaviour
     }
     void GenerteData(){
         questList.Add(10,new QuestData("첫 마을방문",new int[] {100,200}));
+        questList.Add(20,new QuestData("동전주워주기",new int[] {500,200}));
     }
     public int GetQuestTalkIndex(int Id){
-        return questId;
+        return questId+ questActionIndex;
+    }
+    public void CheckQuest(int id){
+        //npc아이디와 퀘스트와 관련된 npc아이디가 같을때만 퀘스트가 진행됨
+        if(id==questList[questId].npcId[questActionIndex]){
+            questActionIndex++;
+        }
+        //퀘스트가 끝나면 다음 퀘스트로 변경
+        if(questActionIndex==questList[questId].npcId.Length)
+            NestQuest();
+        Debug.Log(questList[questId].questName);
+    }
+    public void CheckQuest(){
+        Debug.Log(questList[questId].questName);
+    }
+    void NestQuest(){
+        questId += 10;
+        questActionIndex = 0;
     }
 }
