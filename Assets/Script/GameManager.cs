b0ed8930c95833060d6105525d86c7f6bc7563af
이원찬 <105588857+Leewonchan14@users.noreply.github.com>
2023-02-20 17:14:51 +0900

    게임매니저 주석 추가

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -41,18 +41,25 @@ public class GameManager : MonoBehaviour
         public GameObject ChatPanel;
     }
     void Talk(int id, bool isNPC){
+        //현재 퀘스트 아이디 가져오기
         int questTalkIndex = questManager.GetQuestTalkIndex(id);
+        //퀘스트아이디와 오브젝트아이디를 더한KEY로 대화 가져오기
         string talkStr = talkManager.GetTalk(id+questTalkIndex,talkIndex);
+        //대화가 끝났다면 액션 false, 토크인덱스를 0으로 초기화 (처음대화부터...)
+        //대사가 끝나면 체크퀘스트로 퀘스트 액션 인덱스를 증가시킨다.
         if(talkStr == null) {
             isAction=false;
             talkIndex=0;
+            questManager.CheckQuest(id);
             return;
         }
+        //NPC라면 NPC초상화를 같이 설정
         if(isNPC){
             UI.ChatText.text = talkStr.Split(':')[0];
             portraitImg.color = new Color(1,1,1,1);
             portraitImg.sprite = talkManager.GetSprite(id,int.Parse(talkStr.Split(':')[1]));
-        }else{
+        } //NPC가 아니라면 ( 사물 )
+        else{
             UI.ChatText.text = talkStr;
             portraitImg.color = new Color(1,1,1,0);
         }
